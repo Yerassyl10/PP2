a20751af1944f14c5f98c47df985ae7c04df8a6c
Yerassyl10 <daniyarbekuly.y@gmail.com>
2019-01-29 16:00:00 +0600

    Successfully uploaded some parts of lab1

---
+++ Week1/Task1/Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int[] array2 = new int[n];
+            int cnt = 0;
+            int d = 0;
+
+            for (int i = 0; i < n; i++)
+
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+                for (int k = 2; k < Math.Sqrt(array[i]); k++)
+
+                {
+                    if (array[i] == 2 || array[i] == 3)
+                    {
+                        array2[d] = array[i];
+                        cnt++;
+                        d++;
+                    }
+
+
+                    else
+                            if (array[i] % k == 0 || array[i] == 1)
+
+                    {
+                        break;
+                    }
+
+
+                    else
+                    {
+                        array2[d] = array[i];
+                        d++;
+                        cnt++;
+                    }
+
+
+
+                }
+
+
+            }
+            Console.WriteLine(cnt);
+            for (int j = 0; j < cnt; j++)
+
+            {
+              
+                Console.Write(array2[j] + " ");
+
+            }
+        }
+    }
+}
---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            int[] array2 = new int[n * 2];
+            int j = 0;
+            for (int i=0; i<n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+
+                array2[j] = array[i];
+                array2[j + 1] = array[i]; 
+
+                j = j + 2;
+
+
+            }
+            for (int f = 0; f <(n * 2); f++)
+            {
+
+                Console.Write(array2[f] + " ");
+
+
+            }
+
+        }
+    }
+}
