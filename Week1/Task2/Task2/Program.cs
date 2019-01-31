098ee12db4eb8751f414ef0e4adb1088c2bd73c6
Yerassyl10 <daniyarbekuly.y@gmail.com>
2019-01-31 18:05:55 +0600

    Task2 was done

--- Week1/Task2/Task2/Program.cs
+++ Week1/Task2/Task2/Program.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 
 namespace Task2
-{
+
+{ 
+    class Student
+    {
+        public string name;
+        public int id;
+        public int yearst;
+
+        public Student()
+        {
+            name = Console.ReadLine();
+            id = int.Parse(Console.ReadLine());
+            yearst = int.Parse(Console.ReadLine());
+        }
+
+        public Student(string name, int id, int yearst)
+        {
+            this.name = name;
+            this.id = id;
+            this.yearst = yearst;
+        }
+
+        public override string ToString()
+        {
+            return yearst + " " + name + " " + id;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+        Student st = new Student();
+      
+        Console.WriteLine(st);
 
         }
     }
