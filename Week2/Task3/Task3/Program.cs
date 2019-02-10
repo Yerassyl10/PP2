1ce117c65b6a683fd123e325826a408ffa197e47
Yerassyl10 <daniyarbekuly.y@gmail.com>
2019-02-10 16:36:53 +0600

    Task3 is added

---
+++ Week2/Task3/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Task3
+{
+    class Program
+    {
+        static void PrintSpaces(int level)
+        {
+            for (int i = 0; i < level; i++)
+                Console.Write("  ");
+        }
+
+        static void Dir(DirectoryInfo directory, int level)
+        {
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            foreach (FileInfo file in files)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(file.Name);
+            }
+
+            foreach (DirectoryInfo d in directories)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(d.Name);
+                Dir(d, level + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"/Users/daniyarbekulyyerassyl/Documents/");
+            Dir(d, 0);
+            Console.ReadKey();
+        }
+    }
+}
