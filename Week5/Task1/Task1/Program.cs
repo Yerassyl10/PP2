d6c9d4f004b8655fd2666a711f03f404026d5c1e
Yerassyl10 <daniyarbekuly.y@gmail.com>
2019-02-27 21:34:47 +0600

    Week5 tasks are added

---
+++ Week5/Task1/Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Task1
+{
+
+    [Serializable]
+    public class Complexn
+    {
+        public double realn { get; set; }
+        public double imagn { get; set; }
+
+
+        public Complexn()
+        { }
+
+        public Complexn(double realn, double imagn)
+        {
+            this.realn = realn;
+            this.imagn = imagn;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // объект для сериализации
+            Complexn complexn = new Complexn(1, 2);
+            Console.WriteLine("Complex number is created");
+
+            // передаем в конструктор тип класса
+            XmlSerializer formatter = new XmlSerializer(typeof(Complexn));
+
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream("test.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, complexn);
+
+                Console.WriteLine("The Complex number is serialized");
+            }
+
+            // десериализация
+            using (FileStream fs = new FileStream("test.xml", FileMode.OpenOrCreate))
+            {
+                Complexn newPerson = (Complexn)formatter.Deserialize(fs);
+
+                Console.WriteLine("The Complex number is deserialized");
+                Console.WriteLine("Real number: {0} --- Imaginary Number: {1}", complexn.realn, complexn.imagn);
+            }
+
+
+        }
+    }
+}
---
+++ Week5/Task2/Task2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Task2
+{
+    public class Marks
+    {
+
+        public int mark;
+        public string point;
+        public string str;
+        public Marks() { }
+        public Marks(int mark, string point)
+        {
+            this.mark = mark;
+            this.point = point;
+
+        }
+       
+
+    }
+
+    class Program
+    {
+
+        static string getLetter(int a)
+        {
+            if (a >= 95) return "A";
+            else if (a < 95 && a >= 90) return "A-";
+            else if (a < 90 && 85 <= a) return "B+";
+            else if (a < 85 && 80 <= a) return "B";
+            else if (a < 80 && 75 <= a) return "B-";
+            else if (a < 75 && 70 <= a) return "C+";
+            else if (a < 70 && 65 <= a) return "C";
+            else if (a < 65 && 60 <= a) return "C-";
+            else if (a < 60 && 55 <= a) return "D+";
+            else  if(a < 55 && 50 <= a) return "D-";
+            return "0";
+           
+        }
+
+        static void Main(string[] args)
+        {
+        
+            List<Marks> myList = new List<Marks>();
+            myList.AddRange(new Marks[]{
+                new Marks(95, getLetter(95)),
+                new Marks(83, getLetter(83)),
+                new Marks(60, getLetter(60))
+        });
+            
+
+            //Serialize
+            FileStream fs1 = new FileStream("test.xml", FileMode.OpenOrCreate);
+
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Marks>));
+
+            formatter.Serialize(fs1, myList);
+
+                fs1.Close();
+
+
+            //Deserialize
+            FileStream fs2 = new FileStream("test.xml", FileMode.OpenOrCreate);
+
+            List<Marks> newmarks = (List<Marks>)formatter.Deserialize(fs2);
+
+
+        }
+
+    }  
+
+}
