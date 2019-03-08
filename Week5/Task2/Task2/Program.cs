bd0e98dfa4f3cd5ddd42374c8de51cb74489c220
Yerassyl10 <daniyarbekuly.y@gmail.com>
2019-03-08 12:15:50 +0600

    Task2 is changed

--- Week5/Task2/Task2/Program.cs
+++ Week5/Task2/Task2/Program.cs
@@ -10,69 +10,86 @@ namespace Task2
 {
     public class Marks
     {
+        public int point { get; set; }
+        public string mark { get; set; }
 
-        public int mark;
-        public string point;
-        public string str;
         public Marks() { }
-        public Marks(int mark, string point)
+        public Marks(int point, string mark)
         {
             this.mark = mark;
             this.point = point;
 
         }
-       
-
+        public override string ToString()
+        {
+            return "The point" + this.point +"(" + this.mark + ")";
+        }
     }
 
-    class Program
-    {
 
-        static string getLetter(int a)
+        class Program
         {
-            if (a >= 95) return "A";
-            else if (a < 95 && a >= 90) return "A-";
-            else if (a < 90 && 85 <= a) return "B+";
-            else if (a < 85 && 80 <= a) return "B";
-            else if (a < 80 && 75 <= a) return "B-";
-            else if (a < 75 && 70 <= a) return "C+";
-            else if (a < 70 && 65 <= a) return "C";
-            else if (a < 65 && 60 <= a) return "C-";
-            else if (a < 60 && 55 <= a) return "D+";
-            else  if(a < 55 && 50 <= a) return "D-";
-            return "0";
-           
-        }
+            static string getLetter(int a)
+            {
+                if (a >= 95) return "A";
+                else if (a < 95 && a >= 90) return "A-";
+                else if (a < 90 && 85 <= a) return "B+";
+                else if (a < 85 && 80 <= a) return "B";
+                else if (a < 80 && 75 <= a) return "B-";
+                else if (a < 75 && 70 <= a) return "C+";
+                else if (a < 70 && 65 <= a) return "C";
+                else if (a < 65 && 60 <= a) return "C-";
+                else if (a < 60 && 55 <= a) return "D+";
+                else if (a < 55 && 50 <= a) return "D-";
+                return "Z";
 
-        static void Main(string[] args)
-        {
-        
-            List<Marks> myList = new List<Marks>();
-            myList.AddRange(new Marks[]{
-                new Marks(95, getLetter(95)),
-                new Marks(83, getLetter(83)),
-                new Marks(60, getLetter(60))
-        });
+            }
+
+
+
             
 
-            //Serialize
-            FileStream fs1 = new FileStream("test.xml", FileMode.OpenOrCreate);
 
-            XmlSerializer formatter = new XmlSerializer(typeof(List<Marks>));
+            static void Main(string[] args)
+            {
 
-            formatter.Serialize(fs1, myList);
+                Marks Mark = new Marks();
+                Mark.point = 95;
+                Mark.mark = getLetter(Mark.point);
+
+                Marks Mark2 = new Marks();
+                Mark2.point = 75;
+                Mark2.mark = getLetter(Mark2.point);
+                
+
+                List<string> myList = new List<string>();
+                myList.Add(Mark.ToString());
+                myList.Add(Mark2.ToString());
+
+
+
+
+
+
+                //Serialize
+                FileStream fs1 = new FileStream("test.xml", FileMode.OpenOrCreate);
+
+                XmlSerializer formatter = new XmlSerializer(typeof(List<string>));
+
+                formatter.Serialize(fs1, myList);
 
                 fs1.Close();
 
 
-            //Deserialize
-            FileStream fs2 = new FileStream("test.xml", FileMode.OpenOrCreate);
+                //Deserialize
+                FileStream fs2 = new FileStream("test.xml", FileMode.OpenOrCreate);
 
-            List<Marks> newmarks = (List<Marks>)formatter.Deserialize(fs2);
+                List<string> newmarks = (List<string>)formatter.Deserialize(fs2);
 
+
+            }
 
         }
 
-    }  
-
+    
 }
